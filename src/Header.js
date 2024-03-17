76942e8c12dd3ebb453ce1d07920a1592113f584
AniketBhat1072 <aniketbhat1072@gmail.com>
2024-03-17 07:55:06 +0300

    Routing

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
 
-const Header = function(props){
-    return(
-        <div className='header'>
+const Header = function (props) {
+    return (
+        <div className="header">
             {props.heading}
         </div>
     )
 }
- 
+
 export default Header;
--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,43 +1,35 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
-import './App.css';
+import './ShowSubscribers.css';
+import { Link } from 'react-router-dom';
 
-class App extends Component {
-  deleteHandler(message){
-    alert(message);
+class ShowSubscribers extends Component {
+
+  onDeletedClick = (subscriberId) => {
+    this.props.deleteSubscriberHandler(subscriberId); 
   }
+
   render() {
-
-    let subscribers = [
-      {
-        id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-      }
-    ];
-
     return (
       <div>
         <Header heading="Phone Directory" />
         <div className="component-body-container">
-          <button className="custom-btn add-btn">Add</button>
+          <Link to="/add">
+            <button className="custom-btn add-btn">Add</button>
+          </Link>
+
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
 
           {
-            subscribers.map(sub => {
+            this.props.subscribersList.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
                 <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn" onClick={this.deleteHandler.bind(this, "Delete Clicked")}>Delete</button>
+                  <button className="custom-btn delete-btn" onClick={this.onDeletedClick.bind(this, sub.id)}>Delete</button>
                 </span>
               </div>
             })
@@ -48,5 +40,4 @@ class App extends Component {
   }
 }
 
-
-export default App;
+export default ShowSubscribers;
